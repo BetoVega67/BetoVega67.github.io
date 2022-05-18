9b90f748f7376766a3bb3427e647a5b2ccc2379b
BetoVega67 <104719970+BetoVega67@users.noreply.github.com>
2022-05-17 22:35:21 -0300

    Update controlador.php

--- controlador.php
+++ controlador.php
@@ -1,19 +1,6 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body> 
-        <div id="formulario">
-        </div>
-        
-        <?php
-            $nome = $_POST['nome'];
-            $email = $_POST['email'];
-            $idade = $_POST['idade'];
-
-            include "resposta.php";
-        ?>
-    </body>
+	<body>
+        nome: <?php echo $_POST['nome']; ?>
+	</body>
 </html>
